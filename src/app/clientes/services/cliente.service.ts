ca81a096d22eaf5b4fdf3e54ba478243cba43000
jhooonds <jonatan_ds@msn.com>
2019-11-08 10:04:18 -0300

    services

--- src/app/clientes/services/cliente.service.ts
+++ src/app/clientes/services/cliente.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ClienteI } from "../models/cliente-i";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  private _apiUrl = '10.10.10.173/8080/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  insertCliente(Cliente: ClienteI) {
+    return this.http.post(this._apiUrl, Cliente);
+  }
+
+  getClienteById(id: Number){
+    return this.http.get<ClienteI>(this._apiUrl + '/' + id);
+  }
 }
